00b8cdd76216f064d59cec2f8fca921b22a95d10
NB-N13 <111852815+NB-N13@users.noreply.github.com>
2023-08-10 11:59:20 +0200

    Update Pageload-Nav.js

--- Pageload-Nav.js
+++ Pageload-Nav.js
@@ -1,1 +1,23 @@
+//Page Load Animation
+$(document).ready(function() {
+  $('.loading-screen').show().css('opacity', '1');
 
+  setTimeout(function() {
+    $('.loading-screen').animate({
+      opacity: 0
+    }, 400, 'linear', function() {
+      $('.loading-screen').remove();
+    });
+  }, 100);
+});
+
+//Scrolling Toggle
+$(document).ready(function() {
+  $('[n13-scroll="off"]').click(function() {
+    $('.body-background').addClass('no-scroll');
+  });
+
+  $('[n13-scroll="on"]').click(function() {
+    $('.body-background').removeClass('no-scroll');
+  });
+});
